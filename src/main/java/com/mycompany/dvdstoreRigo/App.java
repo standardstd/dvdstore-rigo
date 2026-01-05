e5b879765a66add7ad81180b8215a3bef9685e04
reaganmampuya <reagan-matondo.mampuya.1@ulaval.ca>
2026-01-04 21:47:41 -0500

    feat: adding autowiring within the applicationContext.xml file

--- src/main/java/com/mycompany/dvdstoreRigo/App.java
+++ src/main/java/com/mycompany/dvdstoreRigo/App.java
@@ -15,6 +15,5 @@ public class App {
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
 
-
     }
 }
--- src/main/java/com/mycompany/dvdstoreRigo/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstoreRigo/repository/FileMovieRepository.java
@@ -2,13 +2,24 @@ package com.mycompany.dvdstoreRigo.repository;
 
 import com.mycompany.dvdstoreRigo.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FileMovieRepository implements IMovieRepository {
-    public void add (Movie movie) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter("movies.txt", true))) {
+    File file;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void add(Movie movie) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(file, true))) {
             writer.println(movie.getTitle() + ";" + movie.getGenre());
             System.out.println("The movie " + movie.getTitle() + " has been added.");
         } catch (IOException e) {
