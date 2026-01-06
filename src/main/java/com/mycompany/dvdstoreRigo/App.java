1430367728314188a3c0661c13dc3713fad613d1
reaganmampuya <reagan-matondo.mampuya.1@ulaval.ca>
2026-01-06 16:16:12 -0500

    feat: using config classes

--- src/main/java/com/mycompany/dvdstoreRigo/App.java
+++ src/main/java/com/mycompany/dvdstoreRigo/App.java
@@ -3,7 +3,6 @@ package com.mycompany.dvdstoreRigo;
 import com.mycompany.dvdstoreRigo.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 /**
@@ -13,7 +12,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
 
-       ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+       ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class, AppConfigPropertySource.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
 
