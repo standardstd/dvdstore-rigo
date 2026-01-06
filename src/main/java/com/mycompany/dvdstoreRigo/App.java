ae81f02ef8bd29eca996e796a42bff86ab7c95f0
reaganmampuya <reagan-matondo.mampuya.1@ulaval.ca>
2026-01-06 16:18:48 -0500

    feat: Make App class a configuration class too

--- src/main/java/com/mycompany/dvdstoreRigo/App.java
+++ src/main/java/com/mycompany/dvdstoreRigo/App.java
@@ -3,12 +3,16 @@ package com.mycompany.dvdstoreRigo;
 import com.mycompany.dvdstoreRigo.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 /**
  * Application class
  * Project entry point
  */
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstoreRigo.controller", "com.mycompany.dvdstoreRigo.repository.file", "com.mycompany.dvdstoreRigo.service"})
 public class App {
     public static void main(String[] args) {
 
--- src/main/java/com/mycompany/dvdstoreRigo/AppConfig.java
+++ src/main/java/com/mycompany/dvdstoreRigo/AppConfig.java
@@ -6,5 +6,6 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.mycompany.dvdstoreRigo.controller", "com.mycompany.dvdstoreRigo.repository.file", "com.mycompany.dvdstoreRigo.service"})
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 }
